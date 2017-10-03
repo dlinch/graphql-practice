65db6de85cb1b4db54f7e7bada458e367a9f508d
dlinch <dlinch33@gmail.com>
2017-10-03 12:17:39 -0600

    graphql returning data properly

--- schema.js
+++ schema.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const util = require('util');
 const parseXML = util.promisify(require('xml2js').parseString);
+require('dotenv').config()
+
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -8,20 +10,17 @@ const {
   GraphQLString
 } = require('graphql')
 
-// fetch(
-//   'https://www.goodreads.com/author/show.xml?id=4432&key=9QxZHmnLmxwJCEpZpPOOA'
-// )
-// .then(response => response.text())
-// .then(parseXML)
-
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   description: '...',
   fields: () => ({
-    name: { type: GraphQLString }
+    name: {
+      type: GraphQLString,
+      resolve: xml =>
+        xml.GoodreadsResponse.author[0].name[0]
+    }
   })
 })
-
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
@@ -31,7 +30,12 @@ module.exports = new GraphQLSchema({
         type: AuthorType,
         args: {
           id: { type: GraphQLInt }
-        }
+        },
+        resolve: (root, args) => fetch(
+            `https://www.goodreads.com/author/show.xml?id=${args.id}&key=${process.env.GOODREADS_KEY}`
+          )
+          .then(response => response.text())
+          .then(parseXML)
       }
     })
   })
